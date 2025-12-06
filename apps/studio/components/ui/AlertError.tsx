459901b4130b2c8d394018b971c37994a6a18d0a
kallebysantos <kalleby_santos@hotmail.com>
2025-12-06 15:04:46 +0000

    feat: improve ai assistant error alert

--- apps/studio/components/ui/AlertError.tsx
+++ apps/studio/components/ui/AlertError.tsx
@@ -13,6 +13,8 @@ export interface AlertErrorProps {
   layout?: 'vertical' | 'horizontal'
   className?: string
   showIcon?: boolean
+  showInstructions?: boolean
+  showErrorPrefix?: boolean
   additionalActions?: React.ReactNode
 }
 
@@ -50,6 +52,8 @@ export const AlertError = ({
   className,
   showIcon = true,
   layout = 'horizontal',
+  showInstructions = true,
+  showErrorPrefix = true,
   children,
   additionalActions,
 }: PropsWithChildren<AlertErrorProps>) => {
@@ -65,11 +69,18 @@ export const AlertError = ({
       title={subject}
       description={
         <>
-          {error?.message && <p>Error: {formattedErrorMessage}</p>}
-          <p>
-            Try refreshing your browser. If the issue persists for more than a few minutes, please
-            reach out to us via support.
-          </p>
+          {error?.message && (
+            <p>
+              {showErrorPrefix && 'Error: '}
+              {formattedErrorMessage}
+            </p>
+          )}
+          {showInstructions && (
+            <p>
+              Try refreshing your browser, but if the issue persists for more than a few minutes,
+              please reach out to us via support.
+            </p>
+          )}
           {children}
         </>
       }
